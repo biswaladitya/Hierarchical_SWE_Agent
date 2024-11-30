8c762d2181ca7f2cb031197af47c3a8be6a51173
julianstrietzel <julian.strietzel@berkeley.edu>
2024-11-29 17:49:06 -0800

    Optimizing edit description to improve modle handling

--- swe-agent/config/commands/edit_linting.sh
+++ swe-agent/config/commands/edit_linting.sh
@@ -1,10 +1,10 @@
 # @yaml
 # signature: |-
-#   edit <start_line>:<end_line>
+#   replace <start_line>:<end_line>
 #   <replacement_text>
 #   end_of_edit
-# docstring: replaces lines <start_line> through <end_line> (inclusive) with the given text in the open file. The replacement text is terminated by a line with only end_of_edit on it. All of the <replacement text> will be entered, so make sure your indentation is formatted properly. Python files will be checked for syntax errors after the edit. If the system detects a syntax error, the edit will not be executed. Simply try to edit the file again, but make sure to read the error message and modify the edit command you issue accordingly. Issuing the same command a second time will just lead to the same error message again.
 # end_name: end_of_edit
+# docstring: replaces lines <start_line> through <end_line> (inclusive) in the open file with the given text (not necessarily the same length). The replacement text is terminated by a line with only end_of_edit on it. All of the <replacement text> will be entered, so make sure your indentation is formatted properly. Python files will be checked for syntax errors after the edit. If the system detects a syntax error, the edit will not be executed. Remember that the lines are replaced by the new content, so make sure to incorporate all previously existing necessary lines in your edit. Check for indenation!
 # arguments:
 #   start_line:
 #     type: integer
@@ -30,18 +30,18 @@ edit() {
 
     if [ -z "$start_line" ] || [ -z "$end_line" ]
     then
-        echo "Usage: edit <start_line>:<end_line>"
+        echo "Usage: replace <start_line>:<end_line>"
         return
     fi
 
     local re='^[0-9]+$'
     if ! [[ $start_line =~ $re ]]; then
-        echo "Usage: edit <start_line>:<end_line>"
+        echo "Usage: replace <start_line>:<end_line>"
         echo "Error: start_line must be a number"
         return
     fi
     if ! [[ $end_line =~ $re ]]; then
-        echo "Usage: edit <start_line>:<end_line>"
+        echo "Usage: replace <start_line>:<end_line>"
         echo "Error: end_line must be a number"
         return
     fi
@@ -85,7 +85,7 @@ edit() {
         _constrain_line
         _print
 
-        echo "File updated. Please review the changes and make sure they are correct (correct indentation, no duplicate lines, etc). Edit the file again if necessary."
+        echo "File updated. Please review the changes and make sure they are correct (correct indentation, no duplicate lines, etc). Edit the file again if necessary (this will be based on the above file)."
     else
         echo "Your proposed edit has introduced new syntax error(s). Please read this error message carefully and then retry editing the file."
         echo ""
@@ -126,6 +126,7 @@ edit() {
 
         echo "Your changes have NOT been applied. Please fix your edit command and try again."
         echo "You either need to 1) Specify the correct start/end line arguments or 2) Correct your edit code."
+        echo "3) Make sure that the open file is the file you wanted to edit."
         echo "DO NOT re-run the same failed edit command. Running it again will lead to the same error."
     fi
 
